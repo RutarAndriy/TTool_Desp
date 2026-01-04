729255843c6c9eac06b802aef39194f931bb55d2
Rutar Andriy <RutarAndriy@gmail.com>
2026-01-04 17:06:20 +0200

    Added CharEntry.java file

---
+++ src/main/java/com/rutar/ttool_desp/CharEntry.java
@@ -0,0 +1,61 @@
+package com.rutar.ttool_desp;
+
+// ............................................................................
+/// Представлення одиночного символу із *.fnt файлу
+/// @author Rutar_Andriy
+/// 04.01.2026
+
+public class CharEntry {
+
+private char charC;  // код символу
+private int charX;      // горизонтальний зсув символу
+private int charW;      // ширина символу
+private int unknownOne; // невідомий параметр 1
+private int unknownTwo; // невідомий параметр 2
+
+// ============================================================================
+/// Конструктор за замовчування
+/// @param charC символ
+/// @param charX горизонтальний зсув символу
+/// @param charW ширина символу
+/// @param unknownOne невідомий параметр 1
+/// @param unknownTwo невідомий параметр 2
+
+public CharEntry (char charC, int charX, int charW, 
+                  int unknownOne, int unknownTwo) {
+    
+    this.charC = charC;
+    this.charX = charX;
+    this.charW = charW;
+    this.unknownOne = unknownOne;
+    this.unknownTwo = unknownTwo;
+}
+
+// ============================================================================
+
+public char getChar() { return charC; }
+public void setChar (char charCode) { this.charC = charCode; }
+
+// ............................................................................
+
+public int getCharX() { return charX; }
+public void setCharX (int charX) { this.charX = charX; }
+
+// ............................................................................
+
+public int getCharW() { return charW; }
+public void setCharW (int charW) { this.charW = charW; }
+
+// ............................................................................
+
+public int getUnknownOne() { return unknownOne; }
+public void setUnknownOne (int unknownOne) { this.unknownOne = unknownOne; }
+
+// ............................................................................
+
+public int getUnknownTwo() { return unknownTwo; }
+public void setUnknownTwo (int unknownTwo) { this.unknownTwo = unknownTwo; }
+
+// Кінець класу CharEntry =====================================================
+
+}
